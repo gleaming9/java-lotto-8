1a893278aad196a2289e8261256aa062089d7e19
gleaming9 <gleaming9@khu.ac.kr>
2025-10-30 17:57:34 +0900

    refactor: 구입 금액 관련 용어를 payment로 통일

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,12 +3,12 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String PAYMENT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public static String getPurchaseAmount(){
-        System.out.println(PURCHASE_AMOUNT_MESSAGE);
+    public static String getPayment(){
+        System.out.println(PAYMENT_MESSAGE);
         return Console.readLine();
     }
 
