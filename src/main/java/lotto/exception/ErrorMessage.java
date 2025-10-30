ee1ef3c47950d53df02321fe1893a103fbf6a665
gleaming9 <gleaming9@khu.ac.kr>
2025-10-30 20:49:50 +0900

    feat: 당첨 번호 관련 에러 메세지 추가
    - INVALID_WINNING_NUMBER_SIZE: 당첨 번호가 6개가 아닌 경우
    - INVALID_WINNING_NUMBER_FORMAT: 당첨 번호에 공백이나 문자가 포함된 경우
    - INVALID_WINNING_NUMBER_RANGE: 당첨 번호가 1~45 사이의 정수가 아닌 경우
    - DUPLICATE_WINNING_NUMBER: 당첨 번호에 중복된 숫자가 포함된 경우

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -3,7 +3,12 @@ package lotto.exception;
 public enum ErrorMessage {
     INSUFFICIENT_PAYMENT("[ERROR] 구입 금액은 1000원 이상이어야 합니다."),
     INVALID_PAYMENT_FORMAT("[ERROR] 구입 금액은 공백이나 문자 없이 숫자로만 입력해야 합니다."),
-    INVALID_PAYMENT_UNIT("[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.");
+    INVALID_PAYMENT_UNIT("[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다."),
+
+    INVALID_WINNING_NUMBER_SIZE("[ERROR] 당첨 번호는 6개여야 합니다."),
+    INVALID_WINNING_NUMBER_FORMAT("[ERROR] 당첨 번호는 공백이나 문자 없이 숫자로만 입력해야 합니다."),
+    INVALID_WINNING_NUMBER_RANGE("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다."),
+    DUPLICATE_WINNING_NUMBER("[ERROR] 당첨 번호에 중복된 숫자가 있을 수 없습니다.");
 
     private final String message;
 
