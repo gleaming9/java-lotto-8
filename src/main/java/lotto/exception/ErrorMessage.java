33800fa0375197348a6e77ddcf070faf4d1d9078
gleaming9 <gleaming9@khu.ac.kr>
2025-10-31 18:45:39 +0900

    feat: 보너스 번호에 대한 에러메세지 갱신
    - 보너스 번호가 유효한 형식이 아닌 경우
    - 보너스 번호가 1~45 사이의 정수가 아닌 경우
    - 보너스 번호가 당첨 번호와 중복될 경우

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -8,7 +8,11 @@ public enum ErrorMessage {
     INVALID_WINNING_NUMBER_SIZE("[ERROR] 당첨 번호는 6개여야 합니다."),
     INVALID_WINNING_NUMBER_FORMAT("[ERROR] 당첨 번호는 공백이나 문자 없이 숫자로만 입력해야 합니다."),
     INVALID_WINNING_NUMBER_RANGE("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다."),
-    DUPLICATE_WINNING_NUMBER("[ERROR] 당첨 번호에 중복된 숫자가 있을 수 없습니다.");
+    DUPLICATE_WINNING_NUMBER("[ERROR] 당첨 번호에 중복된 숫자가 있을 수 없습니다."),
+
+    INVALID_BONUS_NUMBER_FORMAT("[ERROR] 보너스 번호는 공백이나 문자 없이 숫자로만 입력해야 합니다."),
+    INVALID_BONUS_NUMBER_RANGE("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다."),
+    DUPLICATE_BONUS_NUMBER("[ERROR] 보너스 번호는 당첨 번호와 중복된 숫자가 있을 수 없습니다.");
 
     private final String message;
 
