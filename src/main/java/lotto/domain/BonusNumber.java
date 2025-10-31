5fe3e240014a6a96037483b7c4fafcc8357a57d2
gleaming9 <gleaming9@khu.ac.kr>
2025-10-31 18:44:31 +0900

    feat: 보너스 번호를 담는 BonusNumber 클래스 구현

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,50 @@
+package lotto.domain;
+
+import lotto.exception.ErrorMessage;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BonusNumber {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
+    private final int number;
+
+    public BonusNumber(String input, WinningNumber winningNumber) {
+        this.number = validate(input, winningNumber);
+    }
+
+    private int validate(String input, WinningNumber winningNumber) {
+        int num = parseInput(input);
+        validateDuplication(num, winningNumber);
+        return num;
+    }
+
+    private int parseInput(String input) {
+        try {
+            int num = Integer.parseInt(input);
+            validateRange(num);
+            return num;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_BONUS_NUMBER_FORMAT.getMessage());
+        }
+    }
+
+    private void validateRange(int num) {
+        if (num < MIN_LOTTO_NUMBER || num > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_BONUS_NUMBER_RANGE.getMessage());
+        }
+    }
+
+    private void validateDuplication(int num, WinningNumber winningNumber) {
+        if(winningNumber.getNumbers().contains(num)){
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_BONUS_NUMBER.getMessage());
+        }
+    }
+
+    public int getNumber(){
+        return number;
+    }
+}
