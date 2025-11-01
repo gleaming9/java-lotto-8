c95a3281e27e20601187ef366ed84bef244807bf
gleaming9 <gleaming9@khu.ac.kr>
2025-11-02 02:49:58 +0900

    feat: 로또 에러메세지 추가

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -12,7 +12,10 @@ public enum ErrorMessage {
 
     INVALID_BONUS_NUMBER_FORMAT("[ERROR] 보너스 번호는 공백이나 문자 없이 숫자로만 입력해야 합니다."),
     INVALID_BONUS_NUMBER_RANGE("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다."),
-    DUPLICATE_BONUS_NUMBER("[ERROR] 보너스 번호는 당첨 번호와 중복된 숫자가 있을 수 없습니다.");
+    DUPLICATE_BONUS_NUMBER("[ERROR] 보너스 번호는 당첨 번호와 중복된 숫자가 있을 수 없습니다."),
+
+    INVALID_LOTTO_SIZE("[ERROR] 로또 번호는 6개여야 합니다."),
+    DUPLICATE_LOTTO_NUMBER("[ERROR] 로또 번호에 중복된 숫자가 있을 수 없습니다.");
 
     private final String message;
 
