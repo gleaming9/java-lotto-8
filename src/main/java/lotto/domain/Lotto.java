6fa8f8ff0cc849e7aa5d8d3849bd439d38541899
gleaming9 <gleaming9@khu.ac.kr>
2025-11-02 21:47:31 +0900

    feat: 로또 생성 시 번호 정렬 로직 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import lotto.exception.ErrorMessage;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -11,6 +12,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
