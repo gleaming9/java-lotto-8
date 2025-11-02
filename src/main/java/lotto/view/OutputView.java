719280e448c7877c281ebb86d8bc0a6c775f6cf1
gleaming9 <gleaming9@khu.ac.kr>
2025-11-02 21:46:18 +0900

    feat: 구매 개수 및 발행된 로또 번호 출력 기능 추가

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,22 @@
+package lotto.view;
+
+import lotto.domain.Lotto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OutputView {
+    private static final String PAYMENT_COUNT_FORMAT = "\n%d개를 구매했습니다.\n";
+
+    public static void printLotto(List<Lotto> lottos){
+        System.out.printf(PAYMENT_COUNT_FORMAT,lottos.size());
+
+        for(Lotto lotto : lottos){
+            String lottoString = lotto.getNumbers().stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(",","[","]"));
+
+            System.out.println(lottoString);
+        }
+    }
+}
