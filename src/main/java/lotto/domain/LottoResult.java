31d34e60185781537417b6b4c0c51a7fd7c775e6
gleaming9 <gleaming9@khu.ac.kr>
2025-11-02 22:09:41 +0900

    feat: 로또 결과 DTO 구현

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,21 @@
+package lotto.domain;
+
+import java.util.Map;
+
+public class LottoResult {
+    private final Map<LottoRank, Integer> rankCounts;
+    private final double profitRate;
+
+    public LottoResult(Map<LottoRank, Integer> rankCounts, double profitRate){
+        this.rankCounts = rankCounts;
+        this.profitRate = profitRate;
+    }
+
+    public Map<LottoRank, Integer> getRankCounts() {
+        return rankCounts;
+    }
+
+    public double getProfitRate() {
+        return profitRate;
+    }
+}
