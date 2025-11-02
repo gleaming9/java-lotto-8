1a3c686dab58f39a778d0b4457aa7fdf90dedac1
gleaming9 <gleaming9@khu.ac.kr>
2025-11-02 22:55:17 +0900

    feat: 컨트롤러 기본 흐름 및 입력부 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,65 @@
+package lotto.controller;
+
+import lotto.LottoService.LottoCalculateService;
+import lotto.LottoService.LottoInputService;
+import lotto.LottoService.LottoIssuingService;
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
+import lotto.domain.Payment;
+import lotto.domain.WinningNumber;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+public class LottoController {
+
+    LottoIssuingService lottoIssuingService = new LottoIssuingService();
+    LottoCalculateService lottoCalculateService = new LottoCalculateService();
+    LottoInputService lottoInputService = new LottoInputService();
+
+    public void run(){
+        Payment payment = createPayment();
+        List<Lotto> lottos = lottoIssuingService.issueLotto(payment);
+        OutputView.printLotto(lottos);
+
+        WinningNumber winningNumber = createWinningNumber();
+        BonusNumber bonusNumber = createBonusNumber(winningNumber);
+    }
+
+    private Payment createPayment(){
+        while(true){
+            try{
+                String input = InputView.getPayment();
+                return lottoInputService.createPayment(input);
+            }
+            catch(IllegalArgumentException e){
+                OutputView.printError(e.getMessage());
+            }
+        }
+    }
+
+    private WinningNumber createWinningNumber(){
+        while(true){
+            try{
+                String input = InputView.getWinningNumber();
+                return lottoInputService.createWinningNumber(input);
+            }
+            catch(IllegalArgumentException e){
+                OutputView.printError(e.getMessage());
+            }
+        }
+    }
+
+    private BonusNumber createBonusNumber(WinningNumber winningNumber){
+        while(true){
+            try{
+                String input = InputView.getBonusNumber();
+                return lottoInputService.createBonusNumber(input, winningNumber);
+            }
+            catch(IllegalArgumentException e){
+                OutputView.printError(e.getMessage());
+            }
+        }
+    }
+}
