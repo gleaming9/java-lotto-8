67fbc2a37aa1060223541f27eebba57d62419c04
gleaming9 <gleaming9@khu.ac.kr>
2025-11-03 20:38:37 +0900

    feat: 최종 통계 출력 기능 추가 및 로또 출력 형식 올바르게 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,12 +1,18 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
+import lotto.domain.LottoResult;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutputView {
     private static final String PAYMENT_COUNT_FORMAT = "\n%d개를 구매했습니다.\n";
+    private static final String STATISTIC_HEADER = "\n당첨 통계";
+    private static final String STATISTIC_DIVIDER = "---";
+    private static final String STATISTIC_RANK_FORMAT = "%s - %d개\n";
+    private static final String PROFIT_RATE_FORMAT = "총 수익률은 %.1f%%입니다.\n";
 
     public static void printLotto(List<Lotto> lottos){
         System.out.printf(PAYMENT_COUNT_FORMAT,lottos.size());
@@ -14,10 +20,26 @@ public class OutputView {
         for(Lotto lotto : lottos){
             String lottoString = lotto.getNumbers().stream()
                     .map(String::valueOf)
-                    .collect(Collectors.joining(",","[","]"));
+                    .collect(Collectors.joining(", ","[","]"));
 
             System.out.println(lottoString);
         }
+    }
+
+    public static void printResult(LottoResult lottoResult){
+        System.out.println(STATISTIC_HEADER);
+        System.out.println(STATISTIC_DIVIDER);
+
+        for(LottoRank rank : LottoRank.values()){
+            if(rank == LottoRank.MISS){
+                continue;
+            }
+
+            String description = rank.getDescription();
+            int count = lottoResult.getRankCounts().get(rank);
+            System.out.printf(STATISTIC_RANK_FORMAT,description,count);
+        }
+        System.out.printf(PROFIT_RATE_FORMAT,lottoResult.getProfitRate());
     }
 
     public static void printError(String message){
