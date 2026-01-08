b1e805bdf384a89f0b6bebac54f17b664a7a50e6
gleaming9 <gleaming9@khu.ac.kr>
2026-01-08 20:41:27 +0900

    feat(view): 입력 및 출력 기능 갱신
    형식에 알맞게 입력 및 출력 기능이 수행되도록 개선합니다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,9 +3,9 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String PAYMENT_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String WINNING_NUMBER_MESSAGE= "당첨 번호를 입력해 주세요.";
-    private static final String BONUS_NUMBER_MESSAGE= "보너스 번호를 입력해 주세요.";
+    private static final String PAYMENT_MESSAGE = "\n구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_MESSAGE= "\n당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE= "\n보너스 번호를 입력해 주세요.";
 
     public static String getPayment(){
         System.out.println(PAYMENT_MESSAGE);
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,35 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
+import lotto.domain.Rank;
+
+import java.util.List;
+
 public class OutputView {
-    public static void printError(String message){
+    private static final String LOTTO_FORMAT = "%d개를 구매했습니다.\n";
+    private static final String RESULT_HEADER = "당첨 통계\n ---";
+    private static final String RANK_FORMAT = "%s%d개\n";
+    private static final String PROFIT_RATE_FORMAT = "총 수익률은 %.1f%%입니다.";
+
+    public static void printLotto(List<Lotto> lottos) {
+        System.out.printf(LOTTO_FORMAT, lottos.size());
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.lottoString());
+        }
+    }
+
+    public static void printResult(LottoResult lottoResult) {
+        System.out.println(RESULT_HEADER);
+        for (Rank rank : Rank.values()) {
+            if (rank == Rank.MISS) continue;
+            int count = lottoResult.getLottoResult().get(rank);
+            System.out.printf(RANK_FORMAT, rank.getDescription(), count);
+        }
+        System.out.printf(PROFIT_RATE_FORMAT, lottoResult.getProfitRate());
+    }
+
+    public static void printError(String message) {
         System.out.println(message);
     }
 }
